ce74fa06effe4d39dc10f21408426f7b7179ee63
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-12-07 17:27:20 +0700

    GApps

--- derp_X00TD.mk
+++ derp_X00TD.mk
@@ -12,6 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit some common DerpFest stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
