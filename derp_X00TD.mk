ba18e26cb8d787bcbf7547bda619598bfd1068f0
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-12-07 10:30:02 +0700

    Update and rename arrow_X00TD.mk to derp_X00TD.mk

--- derp_X00TD.mk
+++ derp_X00TD.mk
@@ -9,18 +9,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common CherishOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-DEVICE_MAINTAINER := Tiktodz
-ARROW_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_X00TD
+PRODUCT_NAME := derp_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
