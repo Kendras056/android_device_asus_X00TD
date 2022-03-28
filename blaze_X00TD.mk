1e97eee5aefb94cc17368512ea4921dab703e641
Kendras056 <kendras056@gmail.com>
2022-03-28 06:13:11 +0000

    X00TD: Projectblaze Bringup
    - FP > https://github.com/Pixel-Props/raven/commit/1de2b4094ae5b05fcb6c991e35c6a5b1c2f11670
    
    Signed-off-by: Kendras056 <kendras056@gmail.com>
    Change-Id: I2132e31281e76b0c216d44aaa57dfe146761e5fc

--- blaze_X00TD.mk
+++ blaze_X00TD.mk
@@ -9,14 +9,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ProjectBlaze stuff
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Face unlock flag
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Gapps flag
+TARGET_GAPPS_ARCH := arm64
+WITH_GAPPS := true
+
+# Unofficial flag
+BLAZE_MAINTAINER := Tiktodz
+BLAZE_BUILD_TYPE := UNOFFICIAL
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := blaze_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -27,7 +41,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 S3B1.220218.004 8242181 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "asus/WW_X00TD/ASUS_X00T_2:9/QKQ1/17.2017.2012.438-20201203:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/S3B1.220218.004/8242181:user/release-keys"
