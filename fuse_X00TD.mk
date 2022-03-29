8209b272fbad225152d7dec57c3f884fc1468c7c
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2022-03-29 10:52:25 +0700

    X00TD: FusionOS (2/2)

--- fuse_X00TD.mk
+++ fuse_X00TD.mk
@@ -9,27 +9,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common ProjectBlaze stuff
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+# Inherit some common FusionOS stuff
+$(call inherit-product, vendor/fuse/config/common.mk)
+FUSE_BUILD_VARIANT := UNOFFICIAL
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Face unlock flag
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
 # Gapps flag
 TARGET_GAPPS_ARCH := arm64
-
-# Unofficial flag
-BLAZE_MAINTAINER := Tiktodz
-BLAZE_BUILD_TYPE := UNOFFICIAL
+WITH_GAPPS := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := blaze_X00TD
+PRODUCT_NAME := fuse_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
